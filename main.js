bf7ad7506bc5612ea69706bbaa00d5a8c360a6f3
Neil Williams <Neil.Williams227@gmail.com>
2018-10-22 20:40:49 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let score, roundScore, activePlayer;
+
+const player1Score = DocumentType.querySelector("#p1Score");
+const player2Score = DocumentType.querySelector("#p2Score");
+const totScore = DocumentType.querySelector("#tot");
+
+let randNum = Math.floor(Math.random() * 6);
+
+console.log(randNum);
