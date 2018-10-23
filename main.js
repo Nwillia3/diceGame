25501766e1f2619c4200793b2057503a8fcf0ddd
Neil Williams <Neil.Williams227@gmail.com>
2018-10-23 08:05:30 -0400

    almost done:

--- main.js
+++ main.js
@@ -1,15 +1,60 @@
-let score, roundScore, activePlayer;
+let scores, roundScore, activePlayer;
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
 
-const player1Score = document.querySelector("#p1Score");
-const player2Score = document.querySelector("#p2Score");
-const totScore = document.querySelector("#tot");
-// const dices = document.querySelector("#dice");
-let dice = Math.floor(Math.random() * 6 + 1);
+// const score1 = document.querySelector("#current-0");
+// const score2 = document.querySelector("#current-1");
+// const totScore0 = document.querySelector("#tot-0");
+// const totScore1 = document.querySelector("#tot-1");
+const button = document.querySelector("#btn");
+const newBtn = document.querySelector("#new");
+const dices = document.querySelector("#dice");
 
-console.log(dice);
+button.addEventListener("click", rollDice);
+// newBtn.addEventListener("click", newGame);
 
-switch (dice) {
-  case 1:
-    console.log(1);
-    break;
+// function newGame() {
+//   roundScore = [0, 0];
+//   document.getElementById("tot-0").textContent = roundScore[0];
+//   document.getElementById("tot-1").textContent = roundScore[1];
+//   document.getElementById("current-0").textContent = roundScore[0];
+//   document.getElementById("current-1").textContent = roundScore[1];
+// }
+
+function rollDice() {
+  let dice = Math.floor(Math.random() * 6 + 1);
+  dices.src = "images/dice-" + dice + ".png";
+
+  if (dice !== 1) {
+    roundScore += dice;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    nextPlayer();
+  }
 }
+
+function nextPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+
+  document.querySelector("#current-0").textContent = 0;
+  document.querySelector("#current-1").textContent = 0;
+}
+
+document.querySelector("#hold").addEventListener("click", () => {
+  // Add current score to global score
+  scores[activePlayer] += roundScore;
+
+  // update ui to show score
+  document.querySelector("#tot-" + activePlayer).textContent =
+    scores[activePlayer];
+
+  // check if player won the game
+
+  if (scores[activePlayer] >= 10) {
+    document.querySelector("#player-" + activePlayer).innerHTML = "Winner!";
+  } else {
+    nextPlayer();
+  }
+});
---
+++ steps.js
@@ -0,0 +1,20 @@
+// create two total scores vars 
+
+// two current scores vars
+
+// new game button
+DONE
+// dice btn
+DONE
+// 6 dice images
+DONE
+
+
+// functionality 
+
+click dice btn to roll dice
+click new game to start over
+if dice = 1 then next player turn 
+first to 100 wins
+
+
