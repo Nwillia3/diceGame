9f9926ea8ec6751ef26a7bf2e9f20a72171bd585
Neil Williams <Neil.Williams227@gmail.com>
2018-10-22 21:06:22 -0400

    added some query selectors

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 let score, roundScore, activePlayer;
 
-const player1Score = DocumentType.querySelector("#p1Score");
-const player2Score = DocumentType.querySelector("#p2Score");
-const totScore = DocumentType.querySelector("#tot");
+const player1Score = document.querySelector("#p1Score");
+const player2Score = document.querySelector("#p2Score");
+const totScore = document.querySelector("#tot");
+// const dices = document.querySelector("#dice");
+let dice = Math.floor(Math.random() * 6 + 1);
 
-let randNum = Math.floor(Math.random() * 6);
+console.log(dice);
 
-console.log(randNum);
+switch (dice) {
+  case 1:
+    console.log(1);
+    break;
+}
