2677f72e28e02d0a6ca44de966590e4c558ea05b
Neil Williams <Neil.Williams227@gmail.com>
2018-10-23 21:33:27 -0400

    fixed the winner function

--- main.js
+++ main.js
@@ -3,24 +3,27 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-// const score1 = document.querySelector("#current-0");
-// const score2 = document.querySelector("#current-1");
-// const totScore0 = document.querySelector("#tot-0");
-// const totScore1 = document.querySelector("#tot-1");
 const button = document.querySelector("#btn");
 const newBtn = document.querySelector("#new");
 const dices = document.querySelector("#dice");
+const test = document.querySelector(".player-1");
+const test1 = document.querySelector(".player-0");
+const holdBtn = document.querySelector("#hold");
 
 button.addEventListener("click", rollDice);
-// newBtn.addEventListener("click", newGame);
+newBtn.addEventListener("click", newGame);
 
-// function newGame() {
-//   roundScore = [0, 0];
-//   document.getElementById("tot-0").textContent = roundScore[0];
-//   document.getElementById("tot-1").textContent = roundScore[1];
-//   document.getElementById("current-0").textContent = roundScore[0];
-//   document.getElementById("current-1").textContent = roundScore[1];
-// }
+function newGame() {
+  roundScore = [0, 0];
+  document.getElementById("tot-0").textContent = 0;
+  document.getElementById("tot-1").textContent = 0;
+  document.getElementById("current-0").textContent = 0;
+  document.getElementById("current-1").textContent = 0;
+  button.disabled = false;
+
+  document.querySelector("#player-0").textContent = "Player 1";
+  document.querySelector("#player-1").textContent = "Player 2";
+}
 
 function rollDice() {
   let dice = Math.floor(Math.random() * 6 + 1);
@@ -32,6 +35,7 @@ function rollDice() {
   } else {
     nextPlayer();
   }
+  holdBtn.disabled = false;
 }
 
 function nextPlayer() {
@@ -42,9 +46,13 @@ function nextPlayer() {
   document.querySelector("#current-1").textContent = 0;
 }
 
-document.querySelector("#hold").addEventListener("click", () => {
+//Hold button
+holdBtn.addEventListener("click", checkWinner);
+
+function checkWinner() {
   // Add current score to global score
   scores[activePlayer] += roundScore;
+  console.log(roundScore);
 
   // update ui to show score
   document.querySelector("#tot-" + activePlayer).textContent =
@@ -53,8 +61,12 @@ document.querySelector("#hold").addEventListener("click", () => {
   // check if player won the game
 
   if (scores[activePlayer] >= 10) {
-    document.querySelector("#player-" + activePlayer).innerHTML = "Winner!";
+    console.log(activePlayer);
+    document.querySelector("#player-" + activePlayer).textContent = "Winner!";
+    // console
+    button.disabled = true;
+    document.querySelector("#hold").disabled = true;
   } else {
     nextPlayer();
   }
-});
+}
